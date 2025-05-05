4863e54c6e70349247ebc8ac33494381d876b806
Alexandre Lamarre <alexandre.lamarre@suse.com>
2025-05-05 13:28:46 -0400

    dev: add ocb install script
    Signed-off-by: Alexandre Lamarre <alexandre.lamarre@suse.com>

---
+++ scripts/install-ocb.sh
@@ -0,0 +1,12 @@
+# let us download a file with curl on Linux command line #
+
+VERSION="0.120.0" # otel collector version
+ARCH="amd64" # go archicture
+URL="https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/cmd%2Fbuilder%2Fv${VERSION}/ocb_${VERSION}_linux_${ARCH}"
+
+wget -L $URL
+ls -l
+mv "./ocb_${VERSION}_linux_$ARCH" ocb
+chmod +x ocb
+sudo chown -R root:root ./ocb
+sudo mv -v ocb /usr/bin
